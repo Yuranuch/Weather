a386a00e2f775763f6337f333f01ce8fa8b19801
Yuranuch <yura477573@mail.ru>
2019-10-19 11:12:51 +0300

    first markup

---
+++ src/Components/SearchMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./SearchMenu.module.css"
+
+function SearchMenu() {
+    return (
+        <div className={styles.searchMenu}>
+            <input/>
+        </div>
+    );
+}
+
+export default SearchMenu;
